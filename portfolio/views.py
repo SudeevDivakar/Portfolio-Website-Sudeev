374b8ab70d0ec2a82328197f0697462898c70f8f
Sudeev Divakar <sudeev.divakar@gmail.com>
2024-06-25 12:53:21 +0530

    Finish About Me Page

--- portfolio/views.py
+++ portfolio/views.py
@@ -4,24 +4,12 @@ from django.shortcuts import render
 
 all_projects = [
     {
-        "slug": "hike-in-the-mountains",
-        "image": "mountains.jpg",
-        "author": "Maximilian",
-        "date": date(2021, 7, 21),
-        "title": "Mountain Hiking",
-        "excerpt": "There's nothing like the views you get when hiking in the mountains! And I wasn't even prepared for what happened whilst I was enjoying the view!",
+        "slug": "turf-review",
+        "image": "",
+        "title": "Turf Review System",
+        "description": "TurfReview is a comprehensive system for reviewing and exploring turf locations. Whether you're a sports enthusiast, event planner, or someone looking for the perfect turf for your activities, TurfReview has you covered.",
         "content": """
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+          
         """
     },
     {
@@ -67,16 +55,82 @@ all_projects = [
     }
 ]
 
-def get_date(post):
-    return post['date']
+
+"""Content for the About Me Page"""
+
+certifications = [
+    {
+        "certification": "The Web Developer Bootcamp 2024",
+        "link": "https://www.udemy.com/certificate/UC-75f72636-151d-42c7-bc74-8e662b24211e/?utm_campaign=email&utm_medium=email&utm_source=sendgrid.com",
+        "from": "Udemy",
+        "date": "Jan 2024"
+    },
+    {
+        "certification": "Jira Work Management Fundamentals Badge",
+        "link": "https://university.atlassian.com/student/award/fHUqC9HRyzwQ436EDKpUqQzK",
+        "from": "Atlassian",
+        "date": "Nov 2023"
+    },
+    {
+        "certification": "The Git and Github Bootcamp",
+        "link": "https://www.udemy.com/certificate/UC-60f9a0dc-49e8-4c55-9821-61a44828969f/?utm_medium=email&utm_campaign=email&utm_source=sendgrid.com",
+        "from": "Udemy",
+        "date": "Jul 2023"
+    },
+    {
+        "certification": "AWS Skill Badges (AWS Educate)",
+        "link": "https://www.credly.com/users/sudeev-divakar/badges",
+        "from": "AWS Training and Certification",
+        "date": "Jan 2024"
+    }
+]
+
+domain_and_technologies = {
+    "Languages": ["Javascript", "Python", "C", "Java" ],
+    "Frontend Development": ["HTML", "CSS", "Bootstrap", "Bulma", "Javascript", "React", "Material UI", "EJS", "DTL"],
+    "Database/ORM/ODM": ["MongoDB", "MySQL", "Mongoose"],
+    "Backend Development": ["NodeJS", "Express", "Django", "Flask"],
+    "Version Control": ["Git", "Github"],
+    "DevOps": ["Docker", "Kubernetes"],
+    "API and Testing": ["Postman", "Thunder Client"]
+}
+
+education = [
+    {
+        "institute" : "Ryan International School",
+        "year" : "2009 - 2019",
+        "text" : "1st-10th Grade",
+        "link" : "https://www.ryangroup.org/ryaninternational/icse/bangalore/ryan-international-school-kundalahalli"
+    },
+    {
+        "institute" : "Ekya Schools",
+        "year" : "2019 - 2021",
+        "text" : "11th & 12th Grade",
+        "link" : "https://www.ekyaschools.com/itpl/"
+    },
+    {
+        "institute" : "PES University",
+        "year" : "2021 - 2025",
+        "text" : "B.Tech-CSE",
+        "link" : "https://pes.edu/"
+    }
+]
+
+work_experience = [
+    {
+        "company" : "Cuvasol Technologies Private Limited",
+        "duration" : "June 2024 - August 2024 (2 months)",
+        "position" : "Backend Web Development Intern",
+        "technologies" : ["HTML", "CSS", "PHP", "MySQL", "Postman"],
+        "link": "https://www.cuvasol.com"
+    }
+]
+
+"""End Content for the About Me Page"""
 
 # Create your views here.
 def index(request):
-    sorted_projects = sorted(all_projects, key=get_date)
-    latest_projects = sorted_projects[-3:]
-    return render(request, "portfolio/index.html", {
-        "projects": latest_projects
-    })
+    return render(request, "portfolio/index.html")
 
 def projects(request):
     return render(request, "portfolio/all-projects.html",{
@@ -87,4 +141,12 @@ def specific_project(request, slug):
     identified_project = next(project for project in all_projects if project['slug'] == slug)
     return render(request, "portfolio/single-project.html", {
         "project": identified_project
+    })
+
+def about_me(request):
+    return render(request, "portfolio/about-me.html", {
+        "education": education,
+        "work_experience": work_experience,
+        "domain_and_technologies": domain_and_technologies,
+        "certifications": certifications
     })
